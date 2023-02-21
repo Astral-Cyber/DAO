fb9a8429bd6f6c49d9baaf26d4adcf4f22cada81
astral <1723250309@qq.com>
2023-02-21 19:12:48 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,18 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [
       vue(),
     AutoImport({
